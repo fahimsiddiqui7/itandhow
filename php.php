d96496c323a7a9b6906e17002ade35386dbdad3a
fahimsiddiqui7 <66172367+fahimsiddiqui7@users.noreply.github.com>
2020-12-19 00:33:47 +0600

    Update php.php

--- php.php
+++ php.php
@@ -4,3 +4,6 @@ include_once("Home.html")
 
 
 ?>
+heroku buildpacks:set heroku/php
+Buildpack set. Next release on random-app-1234 will use heroku/php.
+Run `git push heroku master` to create a new release using this buildpack.
